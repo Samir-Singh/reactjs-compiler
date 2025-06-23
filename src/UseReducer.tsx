d4da6e632de90015fb926b1f61dd670efdfd16ad
Samir-Singh <samirsingh2000.ss@gmail.com>
2025-06-23 15:31:23 +0530

    useReducer hook implmented

---
+++ src/UseReducer.tsx
@@ -0,0 +1,45 @@
+import { useReducer } from "react";
+
+type Action =
+  | { type: "increment"; payload?: number }
+  | { type: "decrement"; payload?: number }
+  | { type: "reset" };
+
+const UseReducer = () => {
+  const countReducer = (state: { count: number }, action: Action) => {
+    switch (action.type) {
+      case "increment":
+        return {
+          ...state,
+          count: action.payload
+            ? state.count + action.payload
+            : state.count + 1,
+        };
+      case "decrement":
+        return { ...state, count: state.count - 1 };
+      case "reset":
+        return { ...state, count: 0 };
+      default:
+        return state;
+    }
+  };
+
+  const initialState = { count: 0 };
+
+  const [state, dispatch] = useReducer(countReducer, initialState);
+
+  return (
+    <div>
+      <h1>UseReducer Hook</h1>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "increment", payload: 5 })}>
+        Increment By 5
+      </button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+};
+
+export default UseReducer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 
 // import App from "./App.tsx";
 // import UseContext from "./UseContext";
-import ToggleTheme from "./ToggleTheme";
+// import ToggleTheme from "./ToggleTheme";
+import UseReducer from "./UseReducer";
 
 // createRoot(document.getElementById("root")!).render(<App />);
 // createRoot(document.getElementById("root")!).render(<UseContext />);
-createRoot(document.getElementById("root")!).render(<ToggleTheme />);
+// createRoot(document.getElementById("root")!).render(<ToggleTheme />);
+createRoot(document.getElementById("root")!).render(<UseReducer />);
