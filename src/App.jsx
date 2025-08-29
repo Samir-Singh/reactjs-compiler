6ef62686a83bb5c071d44c92a094ecb7c9e030e2
Samir Singh <samir.singh@innovationm.com>
2025-08-29 22:42:44 +0530

    revised useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useMemo, useState } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const multiply = useMemo(() => {
+    console.log("calculating");
+    return count * 5;
+  }, [count]);
+
+  return (
+    <>
+      <button
+        onClick={() => setCount((prev) => prev + 1)}
+        className="border px-2 rounded"
+      >
+        Count : {count}
+      </button>
+
+      <input
+        value={text}
+        onChange={(e) => setText(e?.target?.value)}
+        placeholder="Type something"
+        className="ml-2 border rounded px-2"
+      />
+
+      <span className="ml-2">
+        {count} * 5 : {multiply}
+      </span>
+    </>
+  );
 };
 
 export default App;
