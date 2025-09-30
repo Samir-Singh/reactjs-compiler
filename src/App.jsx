cdf56355e3d3e9c6ae3a119ed20c4b9f2f441f06
Samir Singh <samir@antino.com>
2025-09-30 21:07:53 +0530

    working on caraousel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,5 @@
-import { useState } from "react";
-import ChildComponent from "./components/ChildComponent";
-import useCustomEffect from "./hooks/useCustomEffect";
-
 const App = () => {
-  const [show, setShow] = useState(true);
-  const [count, setCount] = useState(0);
-
-  // useEffect(() => {
-  //   console.log("render");
-
-  //   return () => {
-  //     console.log("return");
-  //   };
-  // });
-
-  useCustomEffect(() => {
-    console.log("render");
-
-    return () => {
-      console.log("return");
-    };
-  }, [show]);
-
-  return (
-    <div>
-      <h1>useEffect</h1>
-      <p>Count : {count}</p>
-      <button
-        className="border py-1 px-5"
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        Inc
-      </button>
-      <button
-        className="border py-1 px-5"
-        onClick={() => setShow((prev) => !prev)}
-      >
-        Toggle
-      </button>
-
-      {show && <ChildComponent />}
-    </div>
-  );
+  return <div>Hello</div>;
 };
 
 export default App;
